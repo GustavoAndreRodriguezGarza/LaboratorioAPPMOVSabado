2a6c82d71b552ddbf844156163cd1f1e09a7b952
GustavoAndreRodriguezGarza <gustavorodriguez1c@gmail.com>
2019-10-12 08:58:08 -0500

    Mensaje de Login

--- ProyectoBase/src/app/app.module.ts
+++ ProyectoBase/src/app/app.module.ts
@@ -9,12 +9,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { HttpClientModule } from '@angular/common/http';
 import { RegistrarPage } from './auth/registrar/registrar.page';
+import { LoginPage } from './auth/login/login.page';
 
 @NgModule({
-  declarations: [ AppComponent, RegistrarPage],
+  declarations: [ AppComponent, RegistrarPage,LoginPage],
   entryComponents: [],
   imports: [BrowserModule,
             IonicModule.forRoot(),
